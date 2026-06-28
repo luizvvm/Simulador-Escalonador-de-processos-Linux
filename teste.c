a32cc9e8aa971f7112d41edf6a23fb5559aee0f8
luizvvm <luizvvm@ic.ufrj.br>
2026-06-28 16:07:01 -0300

    feat: lógica de inserir na fila 1

--- teste.c
+++ teste.c
@@ -18,8 +18,7 @@ typedef struct PCB{
 //fila de processos prontos
 typedef struct no_fila_prontos{
   PCB processos;
-  float quantum;
-  struct no_fila1 *prox;
+  struct no_fila_prontos *prox;
 } no_fila_prontos;
 
 //Primeira fila de processos
@@ -47,7 +46,11 @@ no_fila1* insere (no_fila1 *p, PCB *processo){
   processo_novo = (no_fila1 *) malloc (sizeof(no_fila1));
   processo_novo->quantum = QUANTUM_FILA1;
   processo_novo->processos = *processo;
-  p->prox = processo_novo;
+  if(p == NULL){
+    p = processo_novo;
+  } else{
+    p->prox = processo_novo; //o cara que o p->prox aponta passa a apontar para o processo_novo
+  } 
   processo_novo->prox = NULL;
 
   return processo_novo;
@@ -71,50 +74,84 @@ if(P->quantum == 0){
 }
 }
 
-void processos_prontos(no_fila_prontos inicio_prontos, no_fila_prontos fim_prontos){
+void processos_prontos(no_fila_prontos *pronto_inicio no_fila_prontos *pronto_final){
+    //processo 1
     PCB *processo_1;
     processo_1 = (PCB *) malloc (sizeof(PCB));
     processo_1->PID = 2003;
     processo_1->prioridade = 1;
-    processo_1->tempo_servico
+    processo_1->tempo_servico = 0;
+    processo_1->tempo_execucao = 0;
+    processo_1->tempo_sistema = 0;
+    processo_1->v = 0;
 
-    int prioridade;
-  float tempo_servico;
-  float tempo_execucao;
-  float tempo_sistema;
-  int []v = [-1, 0, 1]; //-1 bloqueado, 0 pronto, 1 em CPU
+    //processo 2
+    PCB *processo_2;
+    processo_2 = (PCB *) malloc (sizeof(PCB));
+    processo_2->PID = 2003;
+    processo_2->prioridade = 1;
+    processo_2->tempo_servico = 0;
+    processo_2->tempo_execucao = 0;
+    processo_2->tempo_sistema = 0;
+    processo_2->v = 0;
+
+    //processo 3
+    PCB *processo_2;
+    processo_2 = (PCB *) malloc (sizeof(PCB));
+    processo_2->PID = 2003;
+    processo_2->prioridade = 1;
+    processo_2->tempo_servico = 0;
+    processo_2->tempo_execucao = 0;
+    processo_2->tempo_sistema = 0;
+    processo_2->v = 0;
+
+    processos_prontos *processo_no_3
+    processo_no_3 = (processos_prontos *) malloc (sizeof(processos_prontos));
+    processo_no_3->processos = *processo_3;
+    processo_no_3->prox = NULL
+
+    pronto_final = processo_no_3;
+
+    processos_prontos *processo_no_2;
+    processo_no_2 = (processos_prontos *) malloc (sizeof(processos_prontos));
+    processo_no_2->processos = *processo_2;
+    processo_no_3->prox = processo_no_3;
+
+    processos_prontos *processo_no_1
+    processo_no_1 = (processos_prontos *) malloc (sizeof(processos_prontos));
+    processo_no_1->processos = *processo_1;
+    processo_no_1->prox = processo_no_2;
+
+    pronto_inicio = processo_no_1;
+}
+
+
+no_fila_prontos insere_prontos(no_fila_prontos *inicio_prontos, no_fila_prontos *fim_prontos, PCB *processo){
 }
 
 int main() {
-    PCB *processo_teste;
-    processo_teste = (PCB *) malloc (sizeof(PCB));
-    processo_teste->PID = 4323;
-    processo_teste->prioridade = 0;
-    processo_teste->tempo_servico = -1;
-    processo_teste->tempo_execucao = 0;
-    processo_teste->tempo_sistema = 0;
-
 no_fila1 *processo_atual;
+no_fila1 *processo_acabou_de_ser_inserido;
 no_fila_prontos *inicio_prontos = NULL;
 no_fila_prontos *fim_prontos = NULL;
 no_fila1 *inicio_1 = NULL;
 no_fila1 *fim_1 = NULL;
 
+processos_prontos(inicio_prontos, fim_prontos);
 
+While(inicio_prontos->prox != NULL){
+    processo_atual = inicio_prontos->prox;
+    if(inicio_1 == NULL){
+        processo_acabou_de_ser_inserido = insere(inicio_1, processo_atual);
+        inicio_1 = processo_acabou_de_ser_inserido;
+        fim_1 = processo_acabou_de_ser_inserido;
+    }
+    else{
+        processo_acabou_de_ser_inserido = insere(fim_1, processo_atual)
+        fim_1 = processo_acabou_de_ser_inserido;
+    }
 
-While()
-
-
-if(inicio_1 == NULL){
-  inicio_1 = (no_fila1 *) malloc (sizeof(no_fila1));
-  fim_1 = inicio_1;
-  inicio_1->prox = NULL;
-  processo_atual = insere(inicio_1, processo_teste);
 }
-else{
-  insere(fim_1, processo_teste);
-}
-
 
 CPU(processo_atual, fim_1, inicio_1);
 
